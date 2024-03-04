324af77355003d8d7f7f25a6c002d9a6b5d58d40
tom wawer <sq5rix@gmail.com>
2024-03-04 11:03:31 +0100

    utils look at camera function

--- utils.py
+++ utils.py
@@ -11,3 +11,16 @@ def clear_scene():
     bpy.ops.object.select_all(action='SELECT')
     bpy.ops.object.delete()
 
+
+def look_at(obj, target_point):
+    """
+    Rotates an object to look at a target point in 3D space.
+
+    Args:
+    - obj: The object to orient.
+    - target_point: The location (Vector) to look at.
+    """
+    # Direction from the object to the target point
+    direction = target_point - obj.location
+    # Point the object's '-Z' and 'Y' towards the target
+    obj.rotation_euler = direction.to_track_quat('-Z', 'Y').to_euler()
